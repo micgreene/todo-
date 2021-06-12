accb1694c29412dbe44800cc8952d84a3d2254e7
micgreene <mgreene11111@yahoo.com>
2021-06-11 20:49:02 -0700

    finished lab31 functionality

--- hooks/src/components/todo/list.js
+++ hooks/src/components/todo/list.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-class TodoList extends React.Component {
+function TodoList(props) {
+ const [tdList, setList] = useState(props.list);
 
-  render() {
-    return (
-      <ul>
-        {this.props.list.map(item => (
-          <li
-            className={`complete-${item.complete.toString()}`}
-            key={item._id}
-          >
-            <span onClick={() => this.props.handleComplete(item._id)}>
-              {item.text}
-            </span>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+ useEffect( () => {
+   setList(props.list);
+ }, [props.list] );
+
+  return (
+    <ul>
+      {tdList.map(item => (        
+        <li
+          className={`complete-${item.complete.toString()}`}
+          key={item._id}
+        >
+          <span onClick={() => props.handleComplete(item._id)}>
+            {item.text}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+
 }
 
 export default TodoList;
